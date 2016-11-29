f9db34399d0d2f3c268d38c21cf4121a3d1ce902
Yan Ke <yansongke1@gmail.com>
2016-11-28 19:57:24 -0500

    start normalizer class

--- TranslatingToCPP/noLibraryCode.cpp
+++ TranslatingToCPP/noLibraryCode.cpp
@@ -120,8 +120,20 @@ public:
     }
 };
 
-//load array
-//split train and labels array
+//load array into arr;
+double arr[3][8]; //arbitrary array
+int arrRow = 3;
+
+double train[arrRow][8];
+double label[arrRow];
+for (int i = 0; i < arrRow; i++)
+{
+    for (int j = 0; j<8; j++)
+    {
+        train[i][j] = arr[i][j];
+    }
+    label[i] = arr[i][8];
+}
 
 int freq = 200;
 double mincutoff = 0.8, beta = 0.4, dcutoff = 1.0;
@@ -135,4 +147,3 @@ while (timestamp < duration)
     double filtered = f.filter(train, timestamp);  //does it take a array value??
     timestamp += 1.0/freq;
 }
-
---
+++ TranslatingToCPP/normalizerFunction.cpp
@@ -0,0 +1,54 @@
+//https://github.com/scikit-learn/scikit-learn/blob/a5ab948/sklearn/preprocessing/data.py#L1377
+#include <string>
+using namespace std;
+
+class Normalizer()
+{
+private:
+	string m_norm;
+	bool m_copy;
+public:
+	Normalizer()
+	{
+		m_norm = "l2";
+		m_copy = true;
+	}
+	//copy should always be true though
+	Normalizer(string norm = "l2", bool copy = true)
+	{
+		m_norm = norm;
+		m_copy = copy;
+	}
+
+	void fit()
+	{
+
+	}
+
+	void transform(double normalize[][9], double x[][9], int rows)
+	{
+		for(int i = 0; i< rows; i++)
+		{
+			double rowSum = 0;
+			for (int j = 0; j < 8; j++)
+			{
+				rowSum+= x[i][j]*x[i][j];
+			}
+			double mag = pow(rowSum, 0.5);
+			for(int j = 0; j<8; j++)
+			{
+				normalize[i][j] = x[i][j]/mag;
+			}
+			normalize[i][8] = x[i][8];
+		}
+	}
+
+	//idk what it returns yet
+	void fit_transform(double x[][9])
+	{
+
+		
+	}
+
+};
+
